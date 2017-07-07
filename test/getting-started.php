850b2c8c771ffd9a5a214e21bd86d64fe2612fb8
Anwar Elven <Anwar Elven>
2017-07-08 00:04:11 +0200

    Added alternative

--- test/getting-started.php
+++ test/getting-started.php
@@ -5,7 +5,23 @@ require __DIR__ . '/../vendor/autoload.php';
 use Khalyomede\Piper;
 use Khalyomede\Add;
 
+function uppercase( $text ) {
+	return ucfirst($text);
+}
+
 echo Piper::set( 'TEST' )
 	->pipe( 'strtolower' )
 	->pipe( function() { return Piper::input()[0] . 'blabla'; } )
+	->pipe('uppercase')
+	->get();
+
+
+
+echo php_sapi_name() === 'cli' ? PHP_EOL : '<br />';
+
+// Can be used without initial set
+echo Piper::pipe( function() { return 'TEST'; } )
+	->pipe('strtolower')
+	->pipe( function() { return Piper::input()[0] . 'blabla'; } )
+	->pipe('uppercase')
 	->get();
