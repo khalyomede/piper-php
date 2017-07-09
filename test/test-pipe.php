aa9dd3ceb1c4ca9120fa4bd330db3acd6915bb3f
Anwar Elven <Anwar Elven>
2017-07-09 14:27:16 +0200

    Added new version of PiperContract

---
+++ test/test-pipe.php
@@ -0,0 +1,15 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Khalyomede\Piper;
+
+function addOne( $input ) {
+	return $input + 1;
+}
+
+Piper::set(13)
+  ->pipe( 'intval' )
+  ->pipe( 'addOne' ) // your previously created function
+  ->pipe( function() { return Piper::input() + 3; } )
+  ->echo();
